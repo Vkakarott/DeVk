b91566e0206db0e6442bbc7e486429f200d16f62
Vkakarott <vecctor.vitor15@gmail.com>
2025-03-23 23:06:01 -0300

    fix: path base

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://vkakarotto.github.io',
-  base: '/DeVk/',
+  site: 'https://vkakarotto.github.io/DeVk/',
+  base: '/',
   vite: {
     plugins: [tailwindcss()]
   }
