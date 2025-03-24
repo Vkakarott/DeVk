60dc815fe2f48a97a517f1c4f3f0e2166fb25537
Vitor Gomes <133702403+Vkakarott@users.noreply.github.com>
2025-03-24 09:19:51 -0300

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://vkakarotto.github.io/DeVk/',
-  base: '/',
+  site: 'https://vkakarotto.github.io/',
   vite: {
     plugins: [tailwindcss()]
   }
